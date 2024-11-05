9f195b0b94e79f55c23cd5f9fda596345f525ba4
Filip Brzozowski <filip.brzozowski@leovegas.com>
2024-11-05 17:53:07 +0100

    Task - 5 Resolved

--- src/test/kotlin/com/gearsofleo/platform/DemoStoreSimulation.kt
+++ src/test/kotlin/com/gearsofleo/platform/DemoStoreSimulation.kt
@@ -3,10 +3,10 @@ package com.gearsofleo.platform
 import io.gatling.javaapi.core.CoreDsl.RawFileBody
 import io.gatling.javaapi.core.CoreDsl.StringBody
 import io.gatling.javaapi.core.CoreDsl.atOnceUsers
+import io.gatling.javaapi.core.CoreDsl.doIf
 import io.gatling.javaapi.core.CoreDsl.exec
 import io.gatling.javaapi.core.CoreDsl.jsonPath
 import io.gatling.javaapi.core.CoreDsl.pause
-import io.gatling.javaapi.core.CoreDsl.repeat
 import io.gatling.javaapi.core.CoreDsl.scenario
 import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.http.HttpDsl.http
@@ -15,24 +15,30 @@ import io.gatling.javaapi.http.HttpDsl.status
 private val authHeader = mapOf(
     "authorization" to "Bearer #{jwtToken}"
 )
+const val isAuthenticated = "authenticated"
+
+private fun initSession() = exec { session -> session.set(isAuthenticated, false) }
 
 object Auth {
-    fun authenticate() = exec(
-        http("Authenticate")
-            .post("/api/authenticate")
-            .body(
-                StringBody(
-                    """
+    fun authenticate() =
+        doIf { session -> !session.getBoolean(isAuthenticated) }.then(
+            exec(
+                http("Authenticate")
+                    .post("/api/authenticate")
+                    .body(
+                        StringBody(
+                            """
                    {
                         "username": "admin",
                         "password": "admin"
                     } 
                 """.trimIndent()
-                )
-            )
-            .check(status().shouldBe(200))
-            .check(jsonPath("$.token").saveAs("jwtToken")),
-    )
+                        )
+                    )
+                    .check(status().shouldBe(200))
+                    .check(jsonPath("$.token").saveAs("jwtToken")),
+            ).exec { s -> s.set(isAuthenticated, true) }
+        )
 }
 
 object Category {
@@ -41,32 +47,41 @@ object Category {
         .check(jsonPath("$[?(@.id == 6)].name").`is`("For Her"))
 
     fun update() =
-        http("Update Category")
-            .put("/api/category/7")
-            .headers(authHeader)
-            .body(RawFileBody("0008_request.json"))
-            .check(jsonPath("$.name").`is`("Everyone"))
+        exec(Auth.authenticate())
+            .exec(
+                http("Update Category")
+                    .put("/api/category/7")
+                    .headers(authHeader)
+                    .body(RawFileBody("0008_request.json"))
+                    .check(jsonPath("$.name").`is`("Everyone"))
+            )
 }
 
 object Product {
     fun list() = http("List Products")
         .get("/api/product?category=7")
 
-    fun update() = http("Update Product")
-        .put("/api/product/17")
-        .headers(authHeader)
-        .body(RawFileBody("0004_request.json"))
-        .check(jsonPath("$.price").`is`("15.99"))
+    fun update() =
+        exec(Auth.authenticate())
+            .exec(
+                http("Update Product")
+                    .put("/api/product/17")
+                    .headers(authHeader)
+                    .body(RawFileBody("0004_request.json"))
+                    .check(jsonPath("$.price").`is`("15.99"))
+            )
 
     fun get() = http("Get Product")
         .get("/api/product/33")
 
-    fun create() = repeat(3, "count").on(
-        http("Create Product #{count}")
-            .post("/api/product")
-            .headers(authHeader)
-            .body(RawFileBody("create_product_#{count}.json"))
-    )
+    fun create() =
+        exec(Auth.authenticate())
+            .repeat(3, "count").on(
+                http("Create Product #{count}")
+                    .post("/api/product")
+                    .headers(authHeader)
+                    .body(RawFileBody("create_product_#{count}.json"))
+            )
 }
 
 class DemoStoreSimulation : Simulation() {
@@ -78,13 +93,13 @@ class DemoStoreSimulation : Simulation() {
 
     private val scn = scenario("DemoStoreSimulation")
         .exec(
+            initSession(),
+            pause(1),
             Category.list(),
             pause(1),
             Product.list(),
             pause(1),
             Product.get(),
-            pause(1),
-            Auth.authenticate(),
             pause(1),
             Product.update(),
             pause(1),
