b5690a7240ebfca61560ddc1d0968efdb38419a9
Filip Brzozowski <filip.brzozowski@leovegas.com>
2024-11-05 20:09:15 +0100

    Task - 8 Resolved

--- src/test/kotlin/com/gearsofleo/platform/DemoStoreSimulation.kt
+++ src/test/kotlin/com/gearsofleo/platform/DemoStoreSimulation.kt
@@ -1,8 +1,9 @@
 package com.gearsofleo.platform
 
+import io.gatling.javaapi.core.Choice
 import io.gatling.javaapi.core.CoreDsl.ElFileBody
 import io.gatling.javaapi.core.CoreDsl.StringBody
-import io.gatling.javaapi.core.CoreDsl.atOnceUsers
+import io.gatling.javaapi.core.CoreDsl.constantConcurrentUsers
 import io.gatling.javaapi.core.CoreDsl.csv
 import io.gatling.javaapi.core.CoreDsl.doIf
 import io.gatling.javaapi.core.CoreDsl.exec
@@ -10,10 +11,13 @@ import io.gatling.javaapi.core.CoreDsl.feed
 import io.gatling.javaapi.core.CoreDsl.jmesPath
 import io.gatling.javaapi.core.CoreDsl.jsonPath
 import io.gatling.javaapi.core.CoreDsl.pause
+import io.gatling.javaapi.core.CoreDsl.rampConcurrentUsers
+import io.gatling.javaapi.core.CoreDsl.randomSwitch
 import io.gatling.javaapi.core.CoreDsl.scenario
 import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.http.HttpDsl.http
 import io.gatling.javaapi.http.HttpDsl.status
+import java.time.Duration
 import kotlin.random.Random
 
 private val authHeader = mapOf(
@@ -70,20 +74,24 @@ object Category {
 object Product {
     private val productFeeder = csv("data/products.csv").circular()
 
+    fun listAll() = exec(
+        http("List All Products").get("/api/product").check(jmesPath("[*]").ofList().saveAs("allProducts"))
+    )
+
     fun list() =
         feed(Category.categoryFeeder)
             .exec(
                 http("List Products")
                     .get("/api/product?category=#{categoryId}")
-                    .check(jmesPath("[*].id").ofList().saveAs("allProducts"))
+                    .check(jmesPath("[*].id").ofList().saveAs("allProductsIds"))
             )
 
     fun get() =
         exec { session ->
-            val allProducts = session.getList<Int>("allProducts")
+            val allProducts = session.getList<Int>("allProductsIds")
             return@exec session.set("productId", allProducts[Random.nextInt(allProducts.size)])
         }.exec { session ->
-            println("All product ids: ${session.getString("allProducts")}")
+            println("All product ids: ${session.getString("allProductsIds")}")
             println("Selected product id: ${session.getString("productId")}")
             return@exec session
         }
@@ -129,31 +137,87 @@ object Product {
         )
 }
 
-class DemoStoreSimulation : Simulation() {
+object UserJourneys {
+    val minPause = Duration.ofMinutes(200)
+    val maxPause = Duration.ofMinutes(3000)
+    val admin = exec(
+        initSession(),
+        Category.list(),
+        pause(minPause, maxPause),
+        Product.list(),
+        pause(minPause, maxPause),
+        Product.get(),
+        pause(minPause, maxPause),
+        Product.update(),
+        pause(minPause, maxPause),
+        Product.create(),
+        pause(minPause, maxPause),
+        Category.update()
+    )
 
+    val priceScraper = exec(
+        Category.list(),
+        pause(1),
+        Product.listAll()
+    )
+
+    val priceUpdated = exec(initSession())
+        .exec(Product.listAll())
+        .repeat("#{allProducts.size()}", "productIndex").on(
+            exec { session ->
+                val index = session.getInt("productIndex")
+                val allProducts = session.getList<Any>("allProducts")
+                return@exec session.set("product", allProducts[index])
+            }.exec(Product.update())
+        )
+}
+
+object Scenarios {
+    val defaultScenario = scenario("DefaultLoadTest")
+        .during(Duration.ofSeconds(60))
+        .on(
+            randomSwitch().on(
+                Choice.WithWeight(20.0, exec(UserJourneys.admin)),
+                Choice.WithWeight(40.0, exec(UserJourneys.priceUpdated)),
+//                Choice.WithWeight(40.0, exec(UserJourneys.priceScraper))
+            )
+        )
+    val noAdmin = scenario("NoAdminLoadTest")
+        .during(Duration.ofSeconds(60))
+        .on(
+            randomSwitch().on(
+                Choice.WithWeight(60.0, exec(UserJourneys.priceUpdated)),
+//                Choice.WithWeight(40.0, exec(UserJourneys.priceScraper))
+            )
+        )
+}
+
+class DemoStoreSimulation : Simulation() {
+    //
     private val httpProtocol = http
         .baseUrl("https://demostore.gatling.io")
         .contentTypeHeader("application/json")
         .acceptHeader("application/json")
-
-    private val scn = scenario("DemoStoreSimulation")
-        .exec(
-            initSession(),
-            pause(1),
-            Category.list(),
-            pause(1),
-            Product.list(),
-            pause(1),
-            Product.get(),
-            pause(1),
-            Product.update(),
-            pause(1),
-            Product.create(),
-            pause(1),
-            Category.update()
-        )
+//
+//    private val scn = scenario("DemoStoreSimulation")
 
     init {
-        setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol)
+        setUp(
+            Scenarios.defaultScenario.injectClosed(
+                rampConcurrentUsers(1).to(5).during(Duration.ofSeconds(30)),
+                constantConcurrentUsers(5).during(Duration.ofSeconds(60))
+            ).protocols(httpProtocol),
+            Scenarios.noAdmin.injectClosed(
+                rampConcurrentUsers(1).to(5).during(Duration.ofSeconds(30)),
+                constantConcurrentUsers(5).during(Duration.ofSeconds(60))
+            ).protocols(httpProtocol)
+        )
+//        setUp(
+//            scn.injectClosed(
+//                rampConcurrentUsers(1).to(50).during(Duration.ofSeconds(20)),
+//                constantConcurrentUsers(100).during(Duration.ofSeconds(5))
+//            ).protocols(httpProtocol)
+//        )
     }
+
 }
